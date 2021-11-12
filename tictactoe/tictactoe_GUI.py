feb06aa05d9f3f2522f3c1d08d2440028bd47ed2
haebbimn <w2003@e-mirim.hs.kr>
2021-11-12 14:58:33 +0900

    tictactoe GUI click_handler, draw_board
    Signed-off-by: haebbimn <w2003@e-mirim.hs.kr>

--- tictactoe/tictactoe_GUI.py
+++ tictactoe/tictactoe_GUI.py
@@ -34,8 +34,7 @@ class TicTactoeGUI:
         row, col = self.coordinate_to_position(event.x, event.y)
         #set row, col
         self.game_engine.set(row, col)
-        #show board
-        self.game_engine.show_board()
+        self.draw_board()
         #set winner
         winner = self.game_engine.set_winner()
         #승자가 있거나 무승부이면 게임오버, 결과 출력
@@ -51,7 +50,20 @@ class TicTactoeGUI:
         self.game_engine.change_turn()
 
     def draw_board(self):
-        pass
+        TILE_SIZE =  self.CANVAS_SIZE // self.game_engine.SIZE #300//3
+        x = 0
+        y = 0
+
+        for i, v in enumerate(self.game_engine.board):
+            if v == '.':
+                pass
+        else:   #elif v == 'X' or v == 'O':
+            self.canvas.create_image(x, y, anchor='nw', image=self.images[v])
+        x += TILE_SIZE
+
+        if i % self.game_engine.SIZE == self.game_engine.SIZE -1:
+            x = 0
+            y += TILE_SIZE
 
     def coordinate_to_position(self, x, y):
         row = y//(self.CANVAS_SIZE // self.game_engine.SIZE) + 1
